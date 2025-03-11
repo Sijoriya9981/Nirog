a12225abb86c8539a595c2624abaf00a04a8e2d4
Vikas Sijoriya <92936175+Sijoriya9981@users.noreply.github.com>
2025-03-11 17:38:58 +0530

    Update authAdmin.js

--- backend/middleware/authAdmin.js
+++ backend/middleware/authAdmin.js
@@ -2,11 +2,11 @@ import jwt from "jsonwebtoken"
 
 // admin authentication middleware
 const authAdmin = async (req, res, next) => {
-    try {
-        const atoken = req.headers['atoken'];  // Get the token from the headers
+   const authHeader = req.headers['authorization'];
+         const atoken = authHeader && authHeader.split(' ')[1]; // Get the token from the headers
     if (!atoken) {
         return res.json({ success: false, message: 'Not Authorized Login Again' });
-    }
+    }     try {
         const token_decode = jwt.verify(atoken, process.env.JWT_SECRET)
         if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
             return res.json({ success: false, message: 'Not Authorized Login Again' })
