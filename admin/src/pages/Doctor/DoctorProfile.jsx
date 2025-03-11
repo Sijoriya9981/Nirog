fb3ec22dc20f61f3cc41cadee25d981e7faecee0
Vikas Sijoriya <92936175+Sijoriya9981@users.noreply.github.com>
2025-03-11 18:09:28 +0530

    Update DoctorProfile.jsx

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -21,7 +21,7 @@ const DoctorProfile = () => {
                 available: profileData.available
             }
 
-            const { data } = await axios.post(backendUrl + '/api/doctor/update-profile', updateData, headers: {
+            const { data } = await axios.post(backendUrl + '/api/doctor/update-profile', updateData,{ headers: {
         Authorization: `Bearer ${dToken}`  // Add the token as a Bearer token
     }
 });
