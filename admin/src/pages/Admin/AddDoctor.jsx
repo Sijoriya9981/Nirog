8c0a4814c32224bb0f29184124fa9041f5d11179
Vikas Sijoriya <92936175+Sijoriya9981@users.noreply.github.com>
2025-03-11 17:55:19 +0530

    Update AddDoctor.jsx

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -49,7 +49,10 @@ const AddDoctor = () => {
                 console.log(`${key}: ${value}`);
             });
 
-            const { data } = await axios.post(backendUrl + '/api/admin/add-doctor', formData, { headers: { aToken } })
+            const { data } = await axios.post(backendUrl + '/api/admin/add-doctor', formData, { headers: {
+        Authorization: `Bearer ${aToken}`  // Add the token as a Bearer token
+    }
+});
             if (data.success) {
                 toast.success(data.message)
                 setDocImg(false)
